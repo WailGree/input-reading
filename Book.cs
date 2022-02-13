ccb9c94d68fc27ff32721537f180d1c1bd3107c2
WailGree <szilagyi.roland.dominik@gmail.com>
2022-02-13 09:06:04 +0100

    Implement input reading from "books.csv"

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿namespace InputReading
+{
+    internal class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int CopiesSold { get; set; }
+        public string Year { get; set; }
+
+        public Book(string line)
+        {
+            string[] data = line.Split('*'); // Split line based on given character. In this case "*".
+            // Set properties from data.
+            Author = data[0];
+            Title = data[1];
+            CopiesSold = int.Parse(data[2]);
+            Year = data[3];
+        }
+        public override string ToString() // Return a fast representation of the Book object's properties.
+        {
+            return $"Author: {Author}, Title: {Title}, CopiesSold: {CopiesSold}, Year: {Year}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace InputReading
 {
@@ -6,7 +9,24 @@ namespace InputReading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Book> books = GetBooks();
+            books.ForEach(book => Console.WriteLine(book));
+            Console.WriteLine($"There are {books.Count()} in the file.");
+            Console.ReadKey();
+        }
+
+        private static List<Book> GetBooks()
+        {
+            // Create a list to collect the books.
+            List<Book> result = new List<Book>();
+
+            foreach (var line in File.ReadAllLines("../../../books.csv").Skip(2)) // Read the lines of "konyvek.csv" from the root folder. Skip the first 2 lines.
+            {
+                Book book = new Book(line); // Create a new book object, with data from the read line.
+                result.Add(book); // Add newly created book to list.
+            }
+
+            return result;
         }
     }
 }
