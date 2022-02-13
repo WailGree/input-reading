b844cce383eb4866632d7b3e2504c9ec391f0dce
WailGree <szilagyi.roland.dominik@gmail.com>
2022-02-13 08:41:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InputReading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
